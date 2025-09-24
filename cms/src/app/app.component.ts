95ff5619b4fad12b948a920595751de66fd08140
Dallin Hale <dallinhale@gmail.com>
2025-09-24 17:45:44 -0600

    W02 - Assignment

---
+++ cms/src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cms-root',
+  templateUrl: './app.component.html',
+  standalone: false,
+  styleUrl: './app.component.css',
+})
+export class AppComponent {}
